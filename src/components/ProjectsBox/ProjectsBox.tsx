e95c6fed1c1f746f3a7e9f9acad5d15345b05212
Karol Proksa <karolproksa@gmail.com>
2022-01-03 23:09:10 +0100

    Link

--- src/components/ProjectsBox/ProjectsBox.tsx
+++ src/components/ProjectsBox/ProjectsBox.tsx
@@ -1,3 +1,4 @@
+import { Link } from "gatsby";
 import React from "react";
 
 const ProjectsBox = (props: any) => {
@@ -9,7 +10,7 @@ const ProjectsBox = (props: any) => {
 			<div className="w-auto mx-[20%] sm:mx-48 mt-20 flex flex-row justify-center flex-wrap-reverse">
 				<div className="flex flex-wrap flex-col">
 					<div className="transition hover:scale-105 hover:drop-shadow-2xl duration-500 w-64 sm:w-[37rem] h-60 m-2 rounded-3xl relative overflow-hidden drop-shadow">
-						<img src={props.Image1} alt=" " className="absolute" />
+						<Link to="/projects"><img src={props.Image1} alt=" " className="absolute" /></Link>
 					</div>
 					<div className="flex flex-wrap flex-row">
 						<div className="transition hover:scale-105 hover:drop-shadow-2xl duration-500 w-64 sm:w-72 h-60 bg-green-400 m-2 rounded-3xl"></div>
