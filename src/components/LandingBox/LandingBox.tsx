7cb3eb0de48f3693e0e4a30bfbf97acec2d741c0
Karol Proksa <karolproksa@gmail.com>
2022-02-14 20:14:55 +0100

    A python script

--- src/components/LandingBox/LandingBox.tsx
+++ src/components/LandingBox/LandingBox.tsx
@@ -19,17 +19,32 @@ const LandingBox = () => {
 					{codeExample === "parlour" && (
 						<div>
 							<div className="flex flex-row justify-around w-[100%] text-stone-500 font-monospace text-[0.8rem] sm:text-lg">
-								<div className="w-4/12 h-10 flex justify-center items-center bg-white rounded-tl-2xl" onClick={() => {setCodeExample("parlour")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-white rounded-tl-2xl"
+									onClick={() => {
+										setCodeExample("parlour");
+									}}
+								>
 									Parlour.tsx
 								</div>
-								<div className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] border-x-[1px] border-stone-200 cursor-pointer" onClick={() => {setCodeExample("script")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] border-x-[1px] border-stone-200 cursor-pointer"
+									onClick={() => {
+										setCodeExample("script");
+									}}
+								>
 									Script.py
 								</div>
-								<div className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] rounded-tr-2xl cursor-pointer" onClick={() => {setCodeExample("less")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] rounded-tr-2xl cursor-pointer"
+									onClick={() => {
+										setCodeExample("less");
+									}}
+								>
 									Less.go
 								</div>
 							</div>
-							<div className="text-left ml-2 mt-2 font-monospace wrap">
+							<div className="text-left ml-2 mt-2 font-monospace truncate">
 								<div className="flex flex-row">
 									<TypeAnimation
 										cursor={false}
@@ -41,19 +56,19 @@ const LandingBox = () => {
 										cursor={false}
 										sequence={[500, " React"]}
 										wrapper="h2"
-										className="ml-1 text-red-500"
+										className="ml-2 text-red-500"
 									/>
 									<TypeAnimation
 										cursor={false}
 										sequence={[1000, "from"]}
 										wrapper="h2"
-										className="ml-1 text-purple-500"
+										className="ml-2 text-purple-500"
 									/>
 									<TypeAnimation
 										cursor={false}
 										sequence={[1200, "'react'"]}
 										wrapper="h2"
-										className="ml-1 text-green-500"
+										className="ml-2 text-green-500"
 									/>
 								</div>
 								<br />
@@ -68,25 +83,25 @@ const LandingBox = () => {
 										cursor={false}
 										sequence={[1800, "Parlour ="]}
 										wrapper="h2"
-										className="ml-1 text-blue-500"
+										className="ml-2 text-blue-500"
 									/>
 									<TypeAnimation
 										cursor={false}
 										sequence={[2300, "()"]}
 										wrapper="h2"
-										className="ml-1 text-purple-500"
+										className="ml-2 text-purple-500"
 									/>
 									<TypeAnimation
 										cursor={false}
 										sequence={[2500, "=>"]}
 										wrapper="h2"
-										className="ml-1 text-purple-500"
+										className="ml-2 text-purple-500"
 									/>
 									<TypeAnimation
 										cursor={false}
 										sequence={[2700, "{"]}
 										wrapper="h2"
-										className="ml-1 text-stone-500"
+										className="ml-2 text-stone-500"
 									/>
 								</div>
 								<div className="flex flex-row">
@@ -100,7 +115,7 @@ const LandingBox = () => {
 										cursor={false}
 										sequence={[2900, "("]}
 										wrapper="h2"
-										className="ml-1 text-stone-500"
+										className="ml-2 text-stone-500"
 									/>
 								</div>
 								<div className="flex flex-row flex-wrap">
@@ -167,13 +182,13 @@ const LandingBox = () => {
 										cursor={false}
 										sequence={[5750, "default"]}
 										wrapper="h2"
-										className="ml-1 text-red-500"
+										className="ml-2 text-red-500"
 									/>
 									<TypeAnimation
 										cursor={false}
 										sequence={[5950, "Parlour"]}
 										wrapper="h2"
-										className="ml-1 text-stone-500"
+										className="ml-2 text-stone-500"
 									/>
 								</div>
 							</div>
@@ -182,37 +197,337 @@ const LandingBox = () => {
 					{codeExample === "script" && (
 						<div>
 							<div className="flex flex-row justify-around w-[100%] text-stone-500 font-monospace text-[0.8rem] sm:text-lg">
-								<div className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] rounded-tl-2xl cursor-pointer" onClick={() => {setCodeExample("parlour")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] rounded-tl-2xl cursor-pointer"
+									onClick={() => {
+										setCodeExample("parlour");
+									}}
+								>
 									Parlour.tsx
 								</div>
-								<div className="w-4/12 h-10 flex justify-center items-center bg-white border-x-[1px] border-stone-200" onClick={() => {setCodeExample("script")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-white border-x-[1px] border-stone-200"
+									onClick={() => {
+										setCodeExample("script");
+									}}
+								>
 									Script.py
 								</div>
-								<div className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] rounded-tr-2xl cursor-pointer" onClick={() => {setCodeExample("less")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] rounded-tr-2xl cursor-pointer"
+									onClick={() => {
+										setCodeExample("less");
+									}}
+								>
 									Less.go
 								</div>
 							</div>
-							<div className="text-left ml-2 mt-2 font-monospace wrap">
-								
+							<div className="text-left ml-2 mt-2 font-monospace truncate">
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={["import"]}
+										wrapper="h2"
+										className="text-purple-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[300, "os"]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+								</div>
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[500, "import"]}
+										wrapper="h2"
+										className="text-purple-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[800, "sys"]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+								</div>
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[1000, "from"]}
+										wrapper="h2"
+										className="text-purple-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[1200, "PIL"]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[1400, "import"]}
+										wrapper="h2"
+										className="ml-2 text-purple-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[1700, "Image"]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+								</div>
+								<br />
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[2000, "if"]}
+										wrapper="h2"
+										className="text-purple-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[2150, "len"]}
+										wrapper="h2"
+										className="ml-2 text-lime-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[2250, "(sys.argv)"]}
+										wrapper="h2"
+										className="text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[2550, ">"]}
+										wrapper="h2"
+										className="ml-2 text-blue-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[2800, "1"]}
+										wrapper="h2"
+										className="ml-2 text-orange-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[2900, ":"]}
+										wrapper="h2"
+										className="text-stone-500"
+									/>
+								</div>
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[3100, "if"]}
+										wrapper="h2"
+										className="ml-8 text-purple-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[3300, "os.path.exists(sys.argv["]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[4000, "1"]}
+										wrapper="h2"
+										className="text-orange-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[4200, "]):"]}
+										wrapper="h2"
+										className="text-stone-500"
+									/>
+								</div>
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[4500, "im"]}
+										wrapper="h2"
+										className="ml-8 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[4500, "="]}
+										wrapper="h2"
+										className="ml-2 text-blue-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[4700, "Image."]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[4950, "open"]}
+										wrapper="h2"
+										className="text-lime-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[5100, "(sys.argv["]}
+										wrapper="h2"
+										className="text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[5400, "1"]}
+										wrapper="h2"
+										className="text-orange-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[5500, "])"]}
+										wrapper="h2"
+										className="text-stone-500"
+									/>
+								</div>
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[5700, "target_name"]}
+										wrapper="h2"
+										className="ml-8 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[6000, "="]}
+										wrapper="h2"
+										className="ml-2 text-blue-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[6200, "sys.argv["]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[6800, "1"]}
+										wrapper="h2"
+										className="text-orange-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[7000, "]"]}
+										wrapper="h2"
+										className="text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[7200, "+"]}
+										wrapper="h2"
+										className="ml-2 text-blue-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[7400, "'.jpg'"]}
+										wrapper="h2"
+										className="ml-2 text-lime-500"
+									/>
+								</div>
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[8000, "rgb_im"]}
+										wrapper="h2"
+										className="ml-8 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[8500, "="]}
+										wrapper="h2"
+										className="ml-2 text-blue-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[8700, "im.convert("]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[9200, "'RGB'"]}
+										wrapper="h2"
+										className="text-lime-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[9600, ")"]}
+										wrapper="h2"
+										className="text-stone-500"
+									/>
+								</div>
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[9800, "rgb_im.save(target_name)"]}
+										wrapper="h2"
+										className="ml-8 text-stone-500"
+									/>
+								</div>
+								<div className="flex flex-row">
+									<TypeAnimation
+										cursor={false}
+										sequence={[11000, "print("]}
+										wrapper="h2"
+										className="ml-8 text-stone-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[11000, "'Saved image as: '"]}
+										wrapper="h2"
+										className="text-lime-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[12000, "+"]}
+										wrapper="h2"
+										className="ml-2 text-blue-500"
+									/>
+									<TypeAnimation
+										cursor={false}
+										sequence={[12200, "target_name)"]}
+										wrapper="h2"
+										className="ml-2 text-stone-500"
+									/>
+								</div>
 							</div>
 						</div>
 					)}
 					{codeExample === "less" && (
 						<div>
 							<div className="flex flex-row justify-around w-[100%] text-stone-500 font-monospace text-[0.8rem] sm:text-lg">
-								<div className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] rounded-tl-2xl cursor-pointer" onClick={() => {setCodeExample("parlour")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] rounded-tl-2xl cursor-pointer"
+									onClick={() => {
+										setCodeExample("parlour");
+									}}
+								>
 									Parlour.tsx
 								</div>
-								<div className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] border-x-[1px] border-stone-200 cursor-pointer" onClick={() => {setCodeExample("script")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-[#f5f5f5] border-x-[1px] border-stone-200 cursor-pointer"
+									onClick={() => {
+										setCodeExample("script");
+									}}
+								>
 									Script.py
 								</div>
-								<div className="w-4/12 h-10 flex justify-center items-center bg-white rounded-tr-2xl" onClick={() => {setCodeExample("less")}}>
+								<div
+									className="w-4/12 h-10 flex justify-center items-center bg-white rounded-tr-2xl"
+									onClick={() => {
+										setCodeExample("less");
+									}}
+								>
 									Less.go
 								</div>
 							</div>
-							<div className="text-left ml-2 mt-2 font-monospace wrap">
-								
-							</div>
+							<div className="text-left ml-2 mt-2 font-monospace truncate"></div>
 						</div>
 					)}
 				</div>
