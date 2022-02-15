e96ab4c5460ec097a0857505702b801710cab1e4
Karol Proksa <karolproksa@gmail.com>
2022-02-15 15:36:51 +0100

    Fix on mible

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -11,12 +11,14 @@ const Carousel = () => {
 		<div className="mx-4 overflow-clip">
 			<Rerousel itemRef={ref} interval={5000}>
 				<div
-					className="flex flex-col justify-center items-center w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl"
+					className="flex flex-col w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl"
 					ref={ref}
 				>
-					<div className="bg-white w-[100%] h-24 z-50 rounded-b-2xl absolute bottom-0 text-left">
-						<p className="mx-8 text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">How to center a div?</p>
-						<p className="mx-8 font-normal">15/02/2022</p>
+					<div className="bg-white w-[100%] z-50 rounded-b-2xl absolute bottom-0 text-left">
+						<p className="mx-8 text-2xl sm:text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">
+							How to center a div?
+						</p>
+						<p className="mx-8 font-normal mb-2">15/02/2022</p>
 					</div>
 					<img
 						src={Ex}
@@ -24,10 +26,12 @@ const Carousel = () => {
 						className="w-[100%] rounded-t-2xl rounded-b-3xl z-10  overflow-hidden"
 					/>
 				</div>
-				<div className="flex flex-col justify-center items-center w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl">
-					<div className="bg-white w-[100%] h-24 z-50 rounded-b-2xl absolute bottom-0 text-left">
-						<p className="mx-8 text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">Mindset Case Study</p>
-						<p className="mx-8 font-normal">15/02/2022</p>
+				<div className="flex flex-col w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl">
+					<div className="bg-white w-[100%] z-50 rounded-b-2xl absolute bottom-0 text-left">
+						<p className="mx-8 text-2xl sm:text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">
+							How to center a div?
+						</p>
+						<p className="mx-8 font-normal mb-2">15/02/2022</p>
 					</div>
 					<img
 						src={Ex}
@@ -35,10 +39,12 @@ const Carousel = () => {
 						className="w-[100%] rounded-t-2xl rounded-b-3xl z-10  overflow-hidden"
 					/>
 				</div>
-				<div className="flex flex-col justify-center items-center w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl">
-					<div className="bg-white w-[100%] h-24 z-50 rounded-b-2xl absolute bottom-0 text-left">
-						<p className="mx-8 text-2xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">10 Cool ReactJS components</p>
-						<p className="mx-8 font-normal">15/02/2022</p>
+				<div className="flex flex-col w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl">
+					<div className="bg-white w-[100%] z-50 rounded-b-2xl absolute bottom-0 text-left">
+						<p className="mx-8 text-2xl sm:text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">
+							How to center a div?
+						</p>
+						<p className="mx-8 font-normal mb-2">15/02/2022</p>
 					</div>
 					<img
 						src={Ex}
@@ -46,10 +52,12 @@ const Carousel = () => {
 						className="w-[100%] rounded-t-2xl rounded-b-3xl z-10  overflow-hidden"
 					/>
 				</div>
-				<div className="flex flex-col justify-center items-center w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl">
-					<div className="bg-white w-[100%] h-24 z-50 rounded-b-2xl absolute bottom-0 text-left">
-						<p className="mx-8 text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">3 lemons witcher</p>
-						<p className="mx-8 font-normal">15/02/2022</p>
+				<div className="flex flex-col w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl">
+					<div className="bg-white w-[100%] z-50 rounded-b-2xl absolute bottom-0 text-left">
+						<p className="mx-8 text-2xl sm:text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">
+							How to center a div?
+						</p>
+						<p className="mx-8 font-normal mb-2">15/02/2022</p>
 					</div>
 					<img
 						src={Ex}
@@ -57,10 +65,12 @@ const Carousel = () => {
 						className="w-[100%] rounded-t-2xl rounded-b-3xl z-10  overflow-hidden"
 					/>
 				</div>
-				<div className="flex flex-col justify-center items-center w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl">
-					<div className="bg-white w-[100%] h-24 z-50 rounded-b-2xl absolute bottom-0 text-left">
-						<p className="mx-8 text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">How to center a div?</p>
-						<p className="mx-8 font-normal">15/02/2022</p>
+				<div className="flex flex-col w-[100%] sm:w-3/12 h-64 mx-2 bg-white  my-1 drop-shadow-sm rounded-2xl">
+					<div className="bg-white w-[100%] z-50 rounded-b-2xl absolute bottom-0 text-left">
+						<p className="mx-8 text-2xl sm:text-3xl font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">
+							How to center a div?
+						</p>
+						<p className="mx-8 font-normal mb-2">15/02/2022</p>
 					</div>
 					<img
 						src={Ex}
