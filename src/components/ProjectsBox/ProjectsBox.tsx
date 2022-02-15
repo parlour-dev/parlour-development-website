fbe149c749ed1592757f8cfd5a0a05f3b366b3f6
Kuba Machnicki <kubamach667@gmail.com>
2022-02-15 12:41:17 +0100

    img fix

--- src/components/ProjectsBox/ProjectsBox.tsx
+++ src/components/ProjectsBox/ProjectsBox.tsx
@@ -29,24 +29,26 @@ const ProjectsBox = (props: any) => {
 					</div>
 				</div>
 			</div> */}
-			<div className="w-auto mx-[20%] sm:mx-48 mt-48 flex flex-row justify-center flex-wrap-reverse">
+			<div className="w-auto mx-[20%]  sm:mx-48 mt-48 flex flex-row justify-center flex-wrap-reverse">
 				<div className="flex flex-col xl:flex-row">
-					<div className="transition hover:scale-105 hover:drop-shadow-2xl duration-500 w-64 sm:w-[37rem] h-60 m-2 rounded-3xl relative overflow-hidden drop-shadow">
+					<div className="relative text-center transition hover:scale-105  hover:drop-shadow-2xl duration-500 w-64 sm:w-[37rem] h-60 m-2 rounded-3xl overflow-hidden drop-shadow">
 						<Link to="/projects">
 							<img
 								src={props.Image1}
 								alt="A picture showing PukPuk - a platform for exchaining services we built"
-								className="absolute object-cover w-[100%] h-[100%]"
+								className="absolute object-cover blur-md sm:blur-none w-[100%] h-[100%]"
 							/>
+						<div className="absolute text-5xl sm:text-[0px] top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] font-bold font-sans text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400" >PukPuk</div>
 						</Link>
 					</div>
-					<div className="transition hover:scale-105 hover:drop-shadow-2xl duration-500 w-64 sm:w-[37rem] h-60 m-2 rounded-3xl relative overflow-hidden">
+					<div className="transition hover:scale-105 hover:drop-shadow-2xl duration-500 w-64 sm:w-[37rem] h-60 m-2 rounded-3xl relative overflow-hidden ">
 						<Link to="/projects#kolor">
 							<img
 								src={props.Image2}
 								alt="A picture showing Kolor - blockchain social media platform we built"
-								className="absolute object-cover w-[100%] h-[100%]"
+								className="absolute object-cover blur-md sm:blur-none w-[100%] h-[100%]"
 							/>
+							<div className="absolute text-5xl sm:text-[0px] top-1/2 left-1/2 translate-y-[-50%] translate-x-[-50%] font-bold font-sans text-transparent bg-clip-text bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400" >Kolor</div>
 						</Link>
 					</div>
 				</div>
