913c5c23c98089a5057824c4350187d012792c1a
Karol Proksa <karolproksa@gmail.com>
2022-02-28 13:00:18 +0100

    Fixed button

--- src/components/SplitSection/SplitSection.tsx
+++ src/components/SplitSection/SplitSection.tsx
@@ -1,7 +1,7 @@
 import { Link } from "gatsby";
 import React from "react";
 import Client from "./img/client.jpg";
-import Testing from "./img/testing.jpg"
+import Testing from "./img/testing.jpg";
 
 const SplitSection = () => {
 	return (
@@ -12,8 +12,15 @@ const SplitSection = () => {
 			<div className="flex flex-col md:flex-row">
 				<div className="flex flex-col items-center justify-center w-auto md:w-[50%] min-h-[90vh] bg-white rounded-2xl ml-4 mr-4 md:mr-2 my-4 drop-shadow-sm">
 					<p className="text-5xl font-bold">Never lose a client</p>
-					<img src={Client} alt="An image of two people conducting business" width="auto" height="auto"/>
-					<p className="text-2xl md:text-4xl">Your website might have poor conversion</p>
+					<img
+						src={Client}
+						alt="An image of two people conducting business"
+						width="auto"
+						height="auto"
+					/>
+					<p className="text-2xl md:text-4xl">
+						Your website might have poor conversion
+					</p>
 				</div>
 				<div className="flex flex-col items-center justify-center w-auto md:w-[50%] min-h-[90vh] bg-white rounded-2xl ml-4 md:ml-2 mr-4 my-4 drop-shadow-sm">
 					<p className="text-5xl font-bold">Let's test it</p>
@@ -24,7 +31,9 @@ const SplitSection = () => {
 						height="auto"
 					/>
 					<Link to="/contact">
-					<button className="w-[80%] mt-16 md:w-96 h-16 hover:scale-105 hover:drop-shadow-lg duration-300 text-2xl font-semibold text-white rounded-full px-6 py-4 bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">Book a free test</button>
+						<button className="w-[100%] mt-16 md:w-96 h-16 hover:scale-105 hover:drop-shadow-lg duration-300 text-2xl font-semibold text-white rounded-full px-6 py-4 bg-gradient-to-r bg-radial-at-tl from-sky-400 via-green-400 to-teal-400">
+							Book a free test
+						</button>
 					</Link>
 				</div>
 			</div>
