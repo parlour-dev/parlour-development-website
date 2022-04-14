c77414fc4b4a30ef66a97594196968e49dcef71a
Karol Proksa <karolproksa@gmail.com>
2022-04-14 15:56:19 +0200

    New website.

--- src/components/Refs/Refs.tsx
+++ src/components/Refs/Refs.tsx
@@ -36,10 +36,10 @@ const Refs = () => {
 						/>
 					</div>
 					<div className=" w-[50%] mt-[88%] md:mt-[95%] mr-[25%] absolute text-white">
-						<p className="text-left text-xl sm:text-4xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">
+						<p className="text-left text-2xl sm:text-4xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">
 							Maria Iwaszko
 						</p>
-						<p className="text-left text-[0px] md:text-xl">
+						<p className="text-left font-medium text-[0px] md:text-xl">
 							Founder at Mindset
 						</p>
 					</div>
@@ -58,34 +58,34 @@ const Refs = () => {
 					<div className="w-[55%] object-scale-down absolute mt-8">
 						<img
 							draggable="false"
-							className="mt-[-2rem] md:mt-0 rounded-[2.5rem] scale-95 md:shadow-2xl shadow-neutral-600 inset-0 absolute  "
+							className="mt-[-2.5rem] md:mt-0 rounded-[2.5rem] scale-95 md:shadow-2xl shadow-neutral-600 inset-0 absolute  "
 							src={GC}
-							alt="Phone image with code writted on its screen"
+							alt="A photo of Grzegorz Ciupek"
 						/>
 					</div>
 					<div className=" w-[50%] mt-[88%] md:mt-[95%] ml-[25%] absolute text-white">
-						<p className="text-left text-xl sm:text-4xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">
+						<p className="text-left text-2xl sm:text-4xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">
 							Grzegorz Ciupek
 						</p>
-						<p className="text-left text-[0px] md:text-xl">
-							CEO {"&"} founder at ECAT
+						<p className="text-left font-medium text-[0px] md:text-xl">
+							CEO {"&"} Founder at ECAT eCommerce
 						</p>
 					</div>
 				</div>
 				<div className="lg:w-6/12  flex flex-col">
 					<p className="lg:mr-20 mt-0 lg:mt-0 leading-tight text-4xl lg:text-5xl xl:text-5xl font-semibold mx-10 lg:mx-auto text-left">
-						I can't picture my company's project being done by any other team.
+						My company's project couldn't have been done better!
 					</p>
 					<p className="lg:mr-20 mt-6 leading-tight text-xl lg:text-xl xl:text-2xl mx-10 lg:mx-auto text-left">
-						Parlour not only developed our product, but helped us understand
-						different approaches to launching an NFT App. They know everything
-						about crypto, both the technicalities and the culture sorrounding
-						it.
+						Parlour not only developed our product, but also helped us understand
+						different approaches to launching an NFT app. They know everything
+						about crypto, both the technicalities and the culture surrounding
+						it. <br /> I am looking forward to working with them again!
 					</p>
 				</div>
 			</div>
 
-			<div className="3xl:mx-auto max-w-screen-3xl flex flex-col overflow-hidden lg:flex-row  w-full mt-36 justify-between items-center ">
+			{/* <div className="3xl:mx-auto max-w-screen-3xl flex flex-col overflow-hidden lg:flex-row  w-full mt-36 justify-between items-center ">
 				<div className="lg:w-6/12  flex flex-col">
 					<p className="lg:ml-20 mt-0 lg:mt-0 leading-tight text-4xl lg:text-5xl xl:text-5xl font-semibold mx-10 lg:mx-auto text-left">
 						Working with Parlour Development has been a great pleasure
@@ -114,15 +114,15 @@ const Refs = () => {
 						/>
 					</div>
 					<div className=" w-[50%] mt-[88%] md:mt-[95%] mr-[25%] absolute text-white">
-						<p className="text-left text-xl sm:text-4xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">
+						<p className="text-left text-2xl sm:text-4xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-semibold">
 							Maria Iwaszko
 						</p>
-						<p className="text-left text-[0px] md:text-xl">
+						<p className="text-left font-medium text-[0px] md:text-xl">
 							Founder at Mindset
 						</p>
 					</div>
 				</div>
-			</div>
+			</div> */}
 			<div className="h-10 bg-[#f5f5f5]"></div>
 		</div>
 	);
